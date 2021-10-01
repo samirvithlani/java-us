d073db0fe410effc65bcbaa87686c85f3af078f6
=samirvithlani <=samir.vithlani83955@gmail.com>
2021-10-01 06:25:06 +0530

    Exception handling introduction

---
+++ src/com/exception/Demo1.java
@@ -0,0 +1,30 @@
+package com.exception;
+
+import java.util.InputMismatchException;
+///we have only package which is not need to import is java.lang
+import java.util.Scanner;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String s =null;
+		System.out.println(s.length());
+		try {
+			System.out.println("pls enter no ");
+			int no = sc.nextInt();
+
+			int ans = 10 / no;
+
+			System.out.println(ans);
+		} catch (ArithmeticException e) {
+
+			System.out.println("cannot divide by zero");
+		} catch (InputMismatchException e) {
+
+			System.out.println("pls check your input..");
+		}
+
+	}
+}
